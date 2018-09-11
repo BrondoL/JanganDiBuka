311b5189598e41c96287df9c6bb331a23437fe20
N0BODy10 <39146007+N0BODy10@users.noreply.github.com>
2018-09-11 09:29:35 +0000

    Add files via upload

---
+++ Nama.cpp
@@ -0,0 +1,17 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	char nama[25];
+
+	cout<<"Masukkan nama : ";
+	gets(nama);
+	cout<<"Nama Kamu adalah "<<nama<<endl;
+	
+	getch();
+	return 0;
+}
