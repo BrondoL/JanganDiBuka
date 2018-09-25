06d8b97456221eba51a2a091176be17a8c67a0a0
N0BODy10 <39146007+N0BODy10@users.noreply.github.com>
2018-09-25 09:10:46 +0700

    Update Konversi_Detik.cpp

--- Konversi_Detik.cpp
+++ Konversi_Detik.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-	int detik, menit, jam;
+	int detik, menit, jam, sisa;
 
 	cout << "\t=====|Konversi Detik|=====";
 	cout << "\n\nBerapa Detik ? ";
 	cin >> detik;
-	jam = detik / 3600;
-	menit = detik / 60;
-	detik = detik % 60;
+	jam = detik / 3600; sisa = detik % 3600;
+	menit = sisa / 60;
+	detik = sisa % 60;
 	cout << endl << jam << "jam : " << menit << "menit : " << detik << "detik"<<endl;
 	cin.ignore().get();
 	system("clear");
