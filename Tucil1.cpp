725a95d2c87c44460b714b09cacf704b730fde3b
N0BODy10 <39146007+N0BODy10@users.noreply.github.com>
2018-10-18 19:11:52 +0700

    Add files via upload

---
+++ Tucil1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+	cout << "\t\t\t==============================================";
+	cout << "\n\t\t\t||           Coded By Abi and Akbar         ||";
+	cout << "\n\t\t\t||                JUDI BERKAH               ||";
+	cout << "\n\t\t\t=============================================="<<endl<<endl;
+    
+    int menu;
+    cout << "\n\nMenu Pilihan :";							
+	cout << "\n\n\t[1]. Login";						
+	cout << "\n\t[2]. Judi Bola";						    
+	cout << "\n\t[3]. Judi roulette";							
+	cout << "\n\t[4]. Exit";
+    cout << "\n\nMasukan Pilihan >> "; cin >> menu; cout<<endl<<endl;
+    //system("cls");
+
+    switch(menu)
+    {
+        case 1:
+        {
+            char nama[25];
+            int points;
+            cout << "====================================";
+	        cout << "\n                LOGIN               ";
+	        cout << "\n===================================="<<endl;
+            cout << "Masukan Username kamu >> " ; cin.getline(nama, sizeof(nama));
+            points = 0;
+        }
+    }
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    return 0;
+
+}
